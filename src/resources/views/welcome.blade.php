50a8e8229889eff18390b935c097c26a146b15e0
Evgeniy Shota <evgeniy.shota@yandex.by>
2025-07-07 15:31:52 +0300

    change deploy action, different small changes

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -4,9 +4,11 @@
             <div class="text-gray-100 mb-2">Welcome!</div>
             <div>
                 Here you can save links to useful (and not so...) resources.
+
                 {{-- and
                 we will try very hard not to lose
                 them. --}}
+
                 We support creating folders, searching and filtering bookmarks
                 by tags.
                 Join in!
