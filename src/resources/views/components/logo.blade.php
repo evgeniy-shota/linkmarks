07ba3e94a8c7d56c614460d2f2a51a0f1ca5e744
Evgeniy Shota <evgeniy.shota@yandex.by>
2025-07-09 22:35:44 +0300

    change logo component, change gitignore in storage/app/public

--- src/resources/views/components/logo.blade.php
+++ src/resources/views/components/logo.blade.php
@@ -1,3 +1,3 @@
 @props(['width' => null, 'height' => null])
-<img src="{{ url('public/storage/linkmarks-logo-gray.png') }}" alt="linkmarks logo"
+<img src="{{ url('storage/linkmarks-logo-gray.png') }}" alt="linkmarks logo"
     srcset="" width="{{ $width ?? '' }}" height="{{ $height ?? '' }}">
