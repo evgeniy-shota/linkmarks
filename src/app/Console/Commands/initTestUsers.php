8e6329dc61a5c1c31fe7a8e477daf573a0498e67
Evgeny <Evgeniy Shota>
2025-07-27 22:42:32 +0300

    Added new default-thumbnails, change phpunut settings.

--- src/app/Console/Commands/initTestUsers.php
+++ src/app/Console/Commands/initTestUsers.php
@@ -164,7 +164,6 @@ class initTestUsers extends Command
                     $user->id
                 );
             }
-            dump(memory_get_peak_usage() / 1024 / 1024 . 'Mb');
         }
     }
 
